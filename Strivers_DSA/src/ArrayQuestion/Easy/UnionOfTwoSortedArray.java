3174d66408561335c9be1a283b25e27e1bc31b68
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-02-10 00:00:26 +0530

    practice

---
+++ Strivers_DSA/src/ArrayQuestion/Easy/UnionOfTwoSortedArray.java
@@ -0,0 +1,17 @@
+package ArrayQuestion.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionOfTwoSortedArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 6};
+        int[] b = {2, 3, 5};
+        System.out.println(sortedArray(a,b));
+    }
+    public static List< Integer > sortedArray(int []a, int []b) {
+        // Write your code here
+        ArrayList<Integer> ls = new ArrayList<>();
+
+    }
+}
---
+++ Strivers_DSA/src/LearnTheBasics/ThingsToKnowInJava/IfElseStatement.java
@@ -0,0 +1,20 @@
+package LearnTheBasics.ThingsToKnowInJava;
+
+import java.sql.SQLOutput;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        System.out.println(compareNM(n,m));
+    }
+    public static String compareNM(int n, int m) {
+        // code here
+        if(n<m){
+            return "Lesser";
+        }else if(n==m){
+            return "equal"
+        }
+        return
+    }
+}
