ba7b5c316563eb6294dcf166a6d092d4b8bcf112
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-02-18 23:43:00 +0530

    First Recurssion

---
+++ Strivers_DSA/src/LearnTheBasics/LearnBasicRecursion/PrintSomethingNtimes.java
@@ -0,0 +1,21 @@
+package LearnTheBasics.LearnBasicRecursion;
+//Without loops
+public class PrintSomethingNtimes {
+    public static void main(String[] args) {
+        int n = 10;
+        printNos(n);
+
+    }public static void printNos(int n) {
+        // Your code here
+        print(1,n);
+
+    }
+    static int print(int i, int n){
+        if(i==n){
+            System.out.println(n);
+            return n;
+        }
+        System.out.print(i+" ");
+        return print(i+1,n);
+    }
+}
