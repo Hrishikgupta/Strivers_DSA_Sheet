e970de2f7dd6e8665fe5940d9d764eed12786637
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-03-09 23:37:07 +0530

    practice

--- Strivers_DSA/src/ArrayQuestion/Easy/MaxConsecutiveOnes.java
+++ Strivers_DSA/src/ArrayQuestion/Easy/MaxConsecutiveOnes.java
@@ -1,4 +1,23 @@
 package ArrayQuestion.Easy;
-
+//leetcode-485
+//https://leetcode.com/problems/max-consecutive-ones/description/
 public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums={1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for(int num: nums){
+            if(num==1){
+                count++;
+            }
+            if(num==0){
+                max = Math.max(count,max);
+                count=0;
+            }
+        }
+        return Math.max(count,max);
+    }
 }
--- Strivers_DSA/src/ArrayQuestion/Easy/MissingNumber.java
+++ Strivers_DSA/src/ArrayQuestion/Easy/MissingNumber.java
@@ -1,4 +1,34 @@
 package ArrayQuestion.Easy;
-
+//leetcode-268
+//https://leetcode.com/problems/missing-number/description/
 public class MissingNumber {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 4, 5};
+        int n = 5;
+        System.out.println(missingNumber(a,n));
+    }
+    public static int missingNumber(int []a, int N) {
+        // Write your code here.
+        int i=0;
+        while(i<a.length){
+            if(a[i]==a.length){
+                i++;
+                continue;
+            }
+            if(a[i]>0 && a[i]<N && a[i]!=a[a[i]-1]){
+                int correctidx = a[i]-1;
+                int temp = a[i];
+                a[i]=a[correctidx];
+                a[correctidx]=temp;
+            }else{
+                i++;
+            }
+        }
+        for(int j=0;j<a.length;j++){
+            if(a[j]!=j+1){
+                return j+1;
+            }
+        }
+        return a.length;
+    }
 }
