b17265e9109a0cabfd94082824f1c920b4aeb492
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-03-15 23:50:53 +0530

    leetcode-75

---
+++ Strivers_DSA/src/ArrayQuestion/Medium/SortAnArrayOf0s1sand2s.java
@@ -0,0 +1,12 @@
+package ArrayQuestion.Medium;
+//Sort an array of 0's , 1's and 2's
+//leetcode-75
+public class SortAnArrayOf0s1sand2s {
+    public static void main(String[] args) {
+        int[] nums = {2,0,2,1,1,0};
+
+    }
+    public static void sortColors(int[] nums) {
+
+    }
+}
