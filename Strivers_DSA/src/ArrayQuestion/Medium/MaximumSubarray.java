33bc0a6ef1f9df4024f1ed559bf74ccf0836f060
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-05 08:46:25 +0530

    array Question

--- Strivers_DSA/src/ArrayQuestion/Medium/MaximumSubarray.java
+++ Strivers_DSA/src/ArrayQuestion/Medium/MaximumSubarray.java
@@ -2,9 +2,21 @@ package ArrayQuestion.Medium;
 
 public class MaximumSubarray {
     public static void main(String[] args) {
-
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
     }
     public static int maxSubArray(int[] nums) {
-
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum>maxi){
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
     }
 }
