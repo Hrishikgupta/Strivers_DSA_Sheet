c23847d7a3169fe483484bdeb0e1ccb3eed132d8
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-15 16:10:55 +0530

    Maximum SubArray Sum

--- Strivers_DSA/src/ArrayQuestion/Medium/MaximumSubarrayII.java
+++ Strivers_DSA/src/ArrayQuestion/Medium/MaximumSubarrayII.java
@@ -2,6 +2,32 @@ package ArrayQuestion.Medium;
 
 public class MaximumSubarrayII {
     public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubarraySum(nums, nums.length));
+    }
+    public static long maxSubarraySum(int[] arr, int n) {
+        long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
 
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
     }
 }
