fc6a84b671a45a506c246fa592a24dddc4dabb33
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-18 22:56:11 +0530

    array Question

--- Strivers_DSA/src/ArrayQuestion/Medium/RearrangeTheArrayInAlternatingPositiveAndNegativeItems.java
+++ Strivers_DSA/src/ArrayQuestion/Medium/RearrangeTheArrayInAlternatingPositiveAndNegativeItems.java
@@ -1,7 +1,13 @@
 package ArrayQuestion.Medium;
 
+import java.util.Arrays;
+
 public class RearrangeTheArrayInAlternatingPositiveAndNegativeItems {
     public static void main(String[] args) {
+        int[] nums = {3,1,-2,-5,2,-4};
+        System.out.println(Arrays.toString(rearrangeArray(nums)));
+    }
+    public static int[] rearrangeArray(int[] nums) {
 
     }
 }
